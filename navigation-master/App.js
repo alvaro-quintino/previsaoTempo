abe7be62cea01ae1813ea3d4a8a429d69154ed19
alvaro-quintino <88858231+alvaro-quintino@users.noreply.github.com>
2022-11-17 21:21:32 -0300

    Add files via upload

---
+++ navigation-master/App.js
@@ -0,0 +1,18 @@
+import { React } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Home from './components/Home';
+import Mongagua from './components/Mongagua'
+
+export default function App() {
+const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Mongagua" component={Mongagua} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )};
